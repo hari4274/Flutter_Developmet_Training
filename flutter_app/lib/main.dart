de36bdeaf889c0af95af78658c5b196c79061435
Hariprasath <bala.hariprasath@gmail.com>
2019-04-24 23:02:30 +0530

    Column

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -75,7 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Container(
-          child: Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               new Text(
                 'Hello world',

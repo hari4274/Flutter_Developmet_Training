34d16358871242ca7f6f4e6aff2f16e899c7d7c9
Hariprasath <bala.hariprasath@gmail.com>
2019-04-25 13:10:56 +0530

    Raised Button

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -125,12 +125,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: FlatButton(
+        child: RaisedButton(
           onPressed: buttonClick,
           child: Text("Click Me"),
           color: Colors.greenAccent,
-          highlightColor: Colors.black,
-          textTheme: ButtonTextTheme.accent,
+          highlightColor: Colors.red,
         ),
       ),
       

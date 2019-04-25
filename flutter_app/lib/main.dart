dc1d5139df085d8c47c60bd676e9dfdf8011b3ad
Hariprasath <bala.hariprasath@gmail.com>
2019-04-25 13:06:50 +0530

    Flat button and functionality

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -74,6 +74,10 @@ class _MyHomePageState extends State<MyHomePage> {
     print("Bottom Nav Item Tapped");
   }
 
+  void buttonClick() {
+    print("Button Clicked");
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -82,29 +86,70 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          pinned: true,
-          expandedHeight: 150,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text("Epic Sliver"),
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        backgroundColor: Colors.cyan,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.call),
+            tooltip: "Call Contact",
+            onPressed: callContact,
           ),
+          IconButton(
+            icon: Icon(Icons.add),
+            tooltip: "Add Information To Contact",
+            onPressed: addInfoToContact,
+          )
+        ],
+      ),
+
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text("Alarm"),
+              onTap: () {
+                // Change the application state
+                print("Change Page");
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
-        SliverFixedExtentList(
-          itemExtent: 50.0,
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index){
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.cyanAccent,
-                child: Text("List Item $index"),
-              );
-            },
-            childCount: 20
-          ),
+      ),
+      
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: FlatButton(
+          onPressed: buttonClick,
+          child: Text("Click Me"),
+          color: Colors.greenAccent,
+          highlightColor: Colors.black,
+          textTheme: ButtonTextTheme.accent,
         ),
-      ],
+      ),
+      
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Image(width: 30, image: AssetImage("assets/images/icon-home.png"),), title: Text("Home")),
+          BottomNavigationBarItem(icon: Icon(Icons.business), title: Text("Business")),
+          BottomNavigationBarItem(icon: Icon(Icons.school), title: Text("School"))
+        ],
+        currentIndex: _selectedIndex,
+        onTap: itemTapped,
+        fixedColor: Colors.amber,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.airplay),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

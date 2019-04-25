2fae7aa90023b98c68d466c09db7277ca8e42f9a
Hariprasath <bala.hariprasath@gmail.com>
2019-04-25 12:23:30 +0530

    Sliver and  SliverAppBar

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -82,64 +82,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        backgroundColor: Colors.cyan,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.call),
-            tooltip: "Call Contact",
-            onPressed: callContact,
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 150,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text("Epic Sliver"),
           ),
-          IconButton(
-            icon: Icon(Icons.add),
-            tooltip: "Add Information To Contact",
-            onPressed: addInfoToContact,
-          )
-        ],
-      ),
-
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.alarm),
-              title: Text("Alarm"),
-              onTap: () {
-                // Change the application state
-                print("Change Page");
-                Navigator.pop(context);
-              },
-            ),
-          ],
         ),
-      ),
-      
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-       
-      ),
-      
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Image(width: 30, image: AssetImage("assets/images/icon-home.png"),), title: Text("Home")),
-          BottomNavigationBarItem(icon: Icon(Icons.business), title: Text("Business")),
-          BottomNavigationBarItem(icon: Icon(Icons.school), title: Text("School"))
-        ],
-        currentIndex: _selectedIndex,
-        onTap: itemTapped,
-        fixedColor: Colors.amber,
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.airplay),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }

41300fcc1465cfba96c2185204010feff2d738dc
Hariprasath <bala.hariprasath@gmail.com>
2019-04-25 09:10:39 +0530

    Align

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -75,7 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Container(
-          child: Center(
+          child: Align(
+            alignment: Alignment.topCenter,
             child: Text("Hello World"),
           ),
         ),

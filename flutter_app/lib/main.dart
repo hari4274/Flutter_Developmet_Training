c09288fd7423cf64625216fa98eb0f00fcf4d230
Hariprasath <bala.hariprasath@gmail.com>
2019-04-25 10:07:25 +0530

    OnDoubleTab event

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -76,8 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Container(
           child: GestureDetector(
-            onTap: (){
-              print("Tapped");
+            onDoubleTap: (){
+              print("DoubleTabed");
             },
             child:Image(image: AssetImage("assets/images/ironman1.jpg")),
           ),

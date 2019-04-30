2ad9d2094329609a174681341ae7f40d3d29c1e7
Hariprasath <bala.hariprasath@gmail.com>
2019-04-30 20:16:37 +0530

    Calculator Part 2 - Implementing The Tabs

--- flutter_app/lib/AddtionTab.dart
+++ flutter_app/lib/AddtionTab.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 class AddtionTab extends StatefulWidget {
   @override
-  _AddtionState _createState() => _AddtionState();
+  _AddtionState createState() => _AddtionState();
 }
 
 class _AddtionState  extends State<AddtionTab>{
+  double num1;
+  double num2;
+  double result = 0;
   @override
   Widget build(BuildContext context) {
-    return Container {
-
-    };
+    return Container (
+      child: Column(
+        children: <Widget>[
+          TextField(
+            onChanged: (String value){
+              num1=double.parse(value);
+            },
+          ),
+          TextField(
+            onChanged: (String value){
+              num2=double.parse(value);
+            },
+          ),
+          RaisedButton(
+            child:Text("Add"),
+            onPressed: () {
+              setState(() {
+                result = num1 + num2;
+              });
+            },
+          ),
+          Text("$result"),
+        ],
+      ),
+    );
   }
   
 }
--- flutter_app/lib/MultiplicationTab.dart
+++ flutter_app/lib/MultiplicationTab.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 class MultiplicationTab extends StatefulWidget {
   @override
-  _MultiplicationState _createState() => _MultiplicationState();
+  _MultiplicationState createState() => _MultiplicationState();
 }
 
 class _MultiplicationState  extends State<MultiplicationTab>{
+  double num1;
+  double num2;
+  double result = 0;
   @override
   Widget build(BuildContext context) {
-    return Container {
-
-    };
+    return Container (
+      child: Column(
+        children: <Widget>[
+          TextField(
+            onChanged: (String value){
+              num1=double.parse(value);
+            },
+          ),
+          TextField(
+            onChanged: (String value){
+              num2=double.parse(value);
+            },
+          ),
+          RaisedButton(
+            child:Text("Multiply"),
+            onPressed: () {
+              setState(() {
+                result = num1 * num2;
+              });
+            },
+          ),
+          Text("$result"),
+        ],
+      ),
+    );
   }
   
 }

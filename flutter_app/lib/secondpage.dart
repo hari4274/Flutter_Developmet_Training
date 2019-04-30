0b3633e2389d6879e3ebc8fb4f479c1404ac59fa
Hariprasath <bala.hariprasath@gmail.com>
2019-04-30 11:58:36 +0530

    Stateless vs Stateful Widgets

--- flutter_app/lib/secondpage.dart
+++ flutter_app/lib/secondpage.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
+  String str = "";
+
+  SecondPage({Key key, this.str}): super(key: key);
+
+  @override
+  _SecondPageState createState() => _SecondPageState(str: str);
+}
+
+class _SecondPageState extends State<SecondPage> {
+  String str = "";
+
+  _SecondPageState({Key key, this.str});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
         appBar: AppBar(
-            title: Text("Second Page Is Awesome")
+            title: Text(str)
         ),
         body: Center(
           child: RaisedButton(
               onPressed: () {
-                Navigator.pop(context);
+                str = "Something else";
+
+                setState(() {
+
+                });
               },
               child: Text("Go Back")
           ),

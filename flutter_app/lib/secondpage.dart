8760c462e5c645723d882f6c96afae554262b68f
Hariprasath <bala.hariprasath@gmail.com>
2019-04-30 11:36:42 +0530

    Navigate with named routes

--- flutter_app/lib/secondpage.dart
+++ flutter_app/lib/secondpage.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-
-  String str = "";
-
-  SecondPage({Key key, this.str}): super(key: key);
-  
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-			appBar: AppBar(
-				title: Text(str),
-			),
-			body: Center(
-        child: RaisedButton(
-          child: Text("Go Back"),
-          onPressed: (){
-            Navigator.pop(context, "Welcome Back " + this.str.toUpperCase());
-          },
+        appBar: AppBar(
+            title: Text("Second Page Is Awesome")
         ),
-      ),
-		);
-  } 
+        body: Center(
+          child: RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Go Back")
+          ),
+        )
+    );
+  }
 }
